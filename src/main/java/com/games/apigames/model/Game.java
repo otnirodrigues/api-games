f24128d7189ef1d3c86281510cc66db1c22206a8
@otni.rodrigues <ors@cin.ufpe.br>
2023-07-11 00:09:57 -0300

    configurando o projeto, criando entity e inserindo no db h2

---
+++ src/main/java/com/games/apigames/model/Game.java
@@ -0,0 +1,36 @@
+package com.games.apigames.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	
+	@Column(name = "game_year")
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
+}
+
